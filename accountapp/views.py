df93a38714ee476b3534f20cf518a9fe6193fcee
MIN JEONG JO <giguda9127@gmail.com>
2021-07-12 11:39:50 +0900

    7/12 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from accountapp.models import HelloWorld
 
 
 def hello_world(request):
@@ -9,7 +9,11 @@ def hello_world(request):
 
         a = request.POST.get('input_text')
 
-        return render(request,'accountapp/hello_world.html', context={'text': a })
+        new_hello_world = HelloWorld()
+        new_hello_world.text = a
+        new_hello_world.save()
+
+        return render(request,'accountapp/hello_world.html', context={'new_hello_world': new_hello_world})
     else:
         return render(request, 'accountapp/hello_world.html', context={'text':'GET METHOD'})
 
