dc62db7588181cedb222c5716c068e3bb844ecdf
MIN JEONG JO <giguda9127@gmail.com>
2021-07-12 12:28:14 +0900

    7/12 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from accountapp.models import HelloWorld
 
@@ -13,9 +14,8 @@ def hello_world(request):
         new_hello_world.text = a
         new_hello_world.save()
 
-        hello_world_list = HelloWorld.objects.all()
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
 
-        return render(request,'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list' : hello_world_list})
