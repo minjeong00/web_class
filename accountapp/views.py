560575e58f4169b7656cc638a4825a0fa2f3296a
MIN JEONG JO <giguda9127@gmail.com>
2021-07-12 11:14:39 +0900

    7/8 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,7 +6,10 @@ from django.shortcuts import render
 
 def hello_world(request):
     if request.method == "POST":
-        return render(request,'accountapp/hello_world.html', context={'text':'POST METHOD'})
+
+        a = request.POST.get('input_text')
+
+        return render(request,'accountapp/hello_world.html', context={'text': a })
     else:
         return render(request, 'accountapp/hello_world.html', context={'text':'GET METHOD'})
 
