1dc15a87b896918a00cf6caa956c57c31267402e
MIN JEONG JO <giguda9127@gmail.com>
2021-07-15 11:16:35 +0900

    7/15 commit

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
 
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
 
     path('create/', AccountCreateView.as_view(), name='create')
 ]
