96ef3d5b282cb8ef075d6473cf4bbb6e195553eb
MIN JEONG JO <giguda9127@gmail.com>
2021-08-24 19:18:04 +0900

    8/24 commit 6

--- temp/settings.py
+++ temp/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.contrib import messages
 from django.urls import reverse_lazy
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,6 +44,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
 
 # Application definition
 
